14074658a83114f8f0cc7e98489b7d1b0f1c4e8c
getsai18 <20243ds167@utez.edu.mx>
2025-12-04 14:20:21 -0600

    Estructura del proyecta

---
+++ src/test/java/com/example/codigito/CodigitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codigito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
